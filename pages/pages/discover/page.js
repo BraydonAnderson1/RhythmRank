f99f302e2ae43163e8fe76fde35e0c863cd8d469
BraydonAnderson1 <129642678+BraydonAnderson1@users.noreply.github.com>
2024-04-23 00:13:03 -0600

    pages

---
+++ pages/pages/discover/page.js
@@ -0,0 +1,143 @@
+'use client';
+
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+const CLIENT_ID = "a7d0f74df3774f789303f7fc77ce014b";
+const REDIRECT_URI = "http://localhost:3000";
+const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+const RESPONSE_TYPE = "token";
+
+function discover() {
+  const [token, setToken] = useState("");
+  const [searchKey, setSearchKey] = useState("")
+  const [artists, setArtists] = useState([])
+
+  useEffect(() => {
+    const hash = window.location.hash
+    let token = window.localStorage.getItem("token")
+
+    if (!token && hash) {
+      token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+
+      window.location.hash = ""
+      window.localStorage.setItem("token", token)
+    }
+
+    setToken(token)
+
+  }, [])
+
+  const logout = () => {
+    setToken("")
+    window.localStorage.removeItem("token")
+  }
+
+  const searchArtists = async (e) => {
+    e.preventDefault()
+    const { data } = await axios.get("https://api.spotify.com/v1/search", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      params: {
+        q: searchKey,
+        type: "artist"
+      }
+    })
+
+    setArtists(data.artists.items)
+  }
+
+  const renderArtists = () => {
+    return artists.map(artist => (
+      <div key={artist.id}>
+        {artist.images.length ? <img width={"10%"} src={artist.images[0].url} alt="" /> : <div>No Image</div>}
+        {artist.name}
+      </div>
+    ))
+  }
+  const fetchTopArtists = async () => {
+    const { data } = await axios.get("https://api.spotify.com/v1/me/top/artists", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      params: {
+        time_range: "short_term",
+        limit: 5,
+        offset: 5
+      }
+    })
+
+    setArtists(data.items)
+  }
+
+  const fetchTopSongs = async () => {
+    const { data } = await axios.get("https://api.spotify.com/v1/me/top/tracks", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      params: {
+        time_range: "short_term",
+        limit: 5,
+        offset: 5
+      }
+    })
+
+    setArtists(data.items)
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="navbar bg-base-100">
+          <div className="flex-1">
+            <a className="btn btn-ghost text-xl">RhythmRank</a>
+          </div>
+          <div className="flex-none gap-2">
+            <div className="form-control">
+              <form onSubmit={searchArtists}>
+                <input type="text" onChange={e => setSearchKey(e.target.value)} className="input input-bordered w-24 md:w-auto" />
+              </form>
+            </div>
+            <div className="dropdown dropdown-end">
+              <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                <div className="w-10 rounded-full">
+                  <span className="text-3xl">B</span>
+                </div>
+              </div>
+              <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+                <li>
+                  <a className="justify-between">
+                    Profile
+                    <span className="badge">New</span>
+                  </a>
+                </li>
+                <li>{!token ?
+                  <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=user-top-read`}>Login to Spotify</a>
+                  : <button onClick={logout}>Logout</button>}</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </header>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">Discover Music</h1>
+            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+            <div className='space-x-0.5'>
+            <button className="btn btn-info">View Top Artists</button>
+            <button className="btn btn-success">View Top Songs</button>
+            <button className="btn btn-error">Discover Music</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        {renderArtists()}
+      </div>
+    </div>
+  );
+}
+
+export default discover;
